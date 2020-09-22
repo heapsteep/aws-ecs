d30ad4918d230744e8c3b9bcb50299550ff10efb
Prasanna Sahu <prasannasahu4u@gmail.com>
2020-09-22 14:34:03 +0530

    first commit

---
+++ src/test/java/com/heapsteep/awsecs/AwsEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heapsteep.awsecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
